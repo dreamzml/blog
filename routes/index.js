ec1cfffbefc92c59509dc3fba52871edd898a65b
dreamzml5 <dream_zml@163.com>
2013-05-07 08:43:03 +0800

    1.0
    生成EXPRESS模块，创建博客首页

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+
+/*
+ * GET home page.
+ */
+
+
+//主页
+exports.index = function(req, res) {
+	res.render('index', { title: 'Express' });
+};
+
+
+//用户主页
+exports.user = function(req, res) {
+};
+
+
+//信息提交
+exports.post = function(req, res) {
+};
+
+
+//用户注册
+exports.reg = function(req, res) {
+};
+
+
+//注册提交
+exports.doReg = function(req, res) {
+};
+
+
+//用户登录
+exports.login = function(req, res) {
+};
+
+
+//登录提交
+exports.doLogin = function(req, res) {
+};
+
+
+//用户退出
+exports.logout = function(req, res) {
+};
---
+++ settings.js
@@ -0,0 +1,6 @@
+	
+module.exports = {
+	cookieSecret: 'blogbyvoid',
+	db: 'blog',
+	host: 'localhost',
+};
